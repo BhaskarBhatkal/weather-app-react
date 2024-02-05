3bd027e0117d691e561e99763cc47a704b71f45c
BhaskarBhatkal <b.r.moger1234@gmail.com>
2024-02-05 23:07:38 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "d75e86ba37msh1fa026948d3f0bfp1a05d0jsna6dd6aaf0659",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "47bf44b99614fca410f3ce3b94a55b7d";
